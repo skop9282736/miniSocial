b9a645d57e534dc1dec40a43b2383ab41bff7da4
slimaidar <slimaidarismail@gmail.com>
2020-04-13 11:25:42 -0400

    Setup react app

--- server.js
+++ server.js
@@ -27,6 +27,6 @@ app.use(passport.initialize())
 require('./config/passport')(passport)
 
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`app running on http://localhost:${port}`))
